0d4f651c5c8c479dd19b2b32a9fff69cf0b0b147
Davidek776 <kocurdavid776@gmail.com>
2022-11-08 14:44:43 +0100

    Added classes in models in App Logic tier

---
+++ BlazorWASM-AppLogic/Domain/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models;
+
+public class Admin
+{
+    public String username { get; set; }
+    public String password { get; set; }
+
+
+    public Admin(string username, string password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ BlazorWASM-AppLogic/Domain/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public abstract class Comment
+{
+    public String body { get; set; }
+
+    protected Comment(string body)
+    {
+        this.body = body;
+    }
+}
