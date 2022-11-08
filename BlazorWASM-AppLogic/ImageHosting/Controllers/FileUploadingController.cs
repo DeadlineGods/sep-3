35105f0b94cbbff52ac95c501a62133d0dda5230
Darko Goluža <darkogoluza@gmail.com>
2022-11-08 15:18:23 +0100

    Created ImageHosting Server

---
+++ BlazorWASM-AppLogic/ImageHosting/Controllers/FileUploadingController.cs
@@ -0,0 +1,47 @@
+﻿using ImageHosting.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageHosting.Controllers;
+
+[ApiController]
+[Route("api/files")]
+public class FileUploadingController : ControllerBase
+{
+    public readonly IWebHostEnvironment env;
+
+    public FileUploadingController(IWebHostEnvironment env)
+    {
+        this.env = env;
+    }
+
+    [HttpPost]
+    public string Post([FromForm] FileUpload objectFile)
+    {
+        try
+        {
+            if (objectFile.Files.Length > 0)
+            {
+                string path = env.WebRootPath + "\\uploads\\";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                using (FileStream fileStream = System.IO.File.Create(path + objectFile.Files.FileName))
+                {
+                    objectFile.Files.CopyTo(fileStream);
+                    fileStream.Flush();
+                    return "Uploaded.";
+                }
+            }
+            else
+            {
+                return "Not Uploaded.";
+            }
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+}
