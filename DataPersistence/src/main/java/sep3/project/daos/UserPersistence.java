03d749e9084563ce7d3540a505fb976e6d69409b
Mateusz Zbyslaw <315192@viauc.dk>
2022-11-28 11:41:30 +0100

    merge with getUsersIdParam

--- DataPersistence/src/main/java/sep3/project/daos/UserPersistence.java
+++ DataPersistence/src/main/java/sep3/project/daos/UserPersistence.java
@@ -1,6 +1,8 @@
 package sep3.project.daos;
 
 import org.springframework.stereotype.Component;
+import sep3.project.protobuf.ResponseGetUsers;
+import sep3.project.protobuf.UserData;
 
 import java.sql.SQLException;
 
