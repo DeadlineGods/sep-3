c3dcae0c884dfc56d29bbd2f7056aa5091df576b
Darko Goluža <darkogoluza@gmail.com>
2022-12-07 23:02:09 +0100

    Added lazy loading when fetching the DBConnection

--- DataPersistence/src/main/java/sep3/project/persistance/DBConnection.java
+++ DataPersistence/src/main/java/sep3/project/persistance/DBConnection.java
@@ -5,20 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-
-	public DBConnection() throws SQLException {
-		DriverManager.registerDriver(new org.postgresql.Driver());
-	}
+	private static Connection connection = null;
 
 	public static Connection getConnection() throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=photoguard";
+		if(connection == null) {
+			DriverManager.registerDriver(new org.postgresql.Driver());
+			String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=photoguard";
 
-		//Remember guys you have to have the same user and password in your postgres for this to work
-		String user = "postgres";
-		String pw = "admin";
+			String user = "postgres";
+			String pw = "admin";
 
-		Connection connection = null;
-		connection = DriverManager.getConnection(url, user, pw);
+			connection = DriverManager.getConnection(url, user, pw);
+		}
 
 		return connection;
 	}
