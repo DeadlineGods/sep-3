e030e2237e286396aa667e2620b0c937a2a244eb
xoxkoo <anton.durcak22@gmail.com>
2022-12-15 14:50:41 +0100

    updating test ddl/dml

--- DataPersistence/src/main/java/sep3/project/static/photoguardtest_DDL_DML.sql
+++ DataPersistence/src/main/java/sep3/project/static/photoguardtest_DDL_DML.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS Location
 	latitude        DECIMAL(14, 10),
 	longitude       DECIMAL(14, 10)
 
-	);
+);
 
 CREATE TABLE IF NOT EXISTS "User"
 (
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS "User"
 	location_id int NOT NULL,
 
 	FOREIGN KEY (location_id) REFERENCES location(id)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS Post
 (
@@ -43,12 +43,12 @@ CREATE TABLE IF NOT EXISTS Post
 	location_id int NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES "User" (id),
 	FOREIGN KEY (location_id) REFERENCES location(id)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS Tag_list
 (
 	tag_name VARCHAR(50) NOT NULL PRIMARY KEY
-	);
+);
 
 CREATE TABLE IF NOT EXISTS Post_tag
 (
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS Post_tag
 	PRIMARY KEY (post_id, tag_name),
 	FOREIGN KEY (post_id) REFERENCES Post (id),
 	FOREIGN KEY (tag_name) REFERENCES Tag_list (tag_name)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS LikePost
 (
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS LikePost
 	PRIMARY KEY (user_id, post_id),
 	FOREIGN KEY (user_id) REFERENCES "User" (id),
 	FOREIGN KEY (post_id) REFERENCES Post (id)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS Comment
 (
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS Comment
 	post_id         int NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES "User" (id),
 	FOREIGN KEY (post_id) REFERENCES Post (id)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS CommentParentComment
 (
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS CommentParentComment
 	PRIMARY KEY (comment_id, parent_comment_id) ,
 	FOREIGN KEY (comment_id) REFERENCES Comment (id),
 	FOREIGN KEY (parent_comment_id) REFERENCES Comment (id)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS Report
 (
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS Report
 	user_id         int NOT NULL,
 	FOREIGN KEY (post_id) REFERENCES Post(id),
 	FOREIGN KEY (user_id) REFERENCES "User"(id)
-	);
+);
 
 
 CREATE TABLE IF NOT EXISTS Admin
@@ -114,7 +114,7 @@ CREATE TABLE IF NOT EXISTS Ban_user
 	user_id         int,
 	FOREIGN KEY (admin) REFERENCES Admin(user_name),
 	FOREIGN KEY (user_id) REFERENCES "User" (id)
-	);
+);
 
 CREATE TABLE IF NOT EXISTS Ban_post
 (
@@ -124,33 +124,32 @@ CREATE TABLE IF NOT EXISTS Ban_post
 	post_id         int,
 	FOREIGN KEY (admin) REFERENCES Admin(user_name),
 	FOREIGN KEY (post_id) REFERENCES Post (id)
-	);
+);
 
 set schema 'photoguardtest';
 
 INSERT INTO location (address, postcode, country, town, latitude, longitude)
-VALUES ('Nygade 45', 8700, 'DK', 'Horsens', 55.858131, 9.847588);
+VALUES ('Kemp St10', 6022, 'NZ', 'Wellington', -41.3154278274, 174.7962668968);
 
 INSERT INTO "User" (user_name, first_name, last_name, password, email, phone_number, location_id)
 VALUES ('Mira','Mirko','Mirkovic','123','miro123@gmail.com','+4576456789', 1);
 INSERT INTO "User" (user_name, first_name, last_name, password, email, phone_number, location_id)
 VALUES ('Dara','Darko','Goluza','123','darkogoluza@gmail.com','+4554325623', 1);
 
-
 INSERT INTO Post (user_id, image_url, title, description, location_id)
-VALUES (1,'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png', 'Test Post', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 1);
-
-INSERT INTO Post (user_id, image_url, title, description, location_id)
-VALUES (2,'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png', 'Test Post 2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 1);
-
-INSERT INTO Post (user_id, image_url, title, description, location_id)
-VALUES (2,'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png', 'Test Post 3', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 1);
+VALUES (1,'https://images.unsplash.com/photo-1655574080225-9400f4e6d1ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 'Trash on the beach in New Zealand', 'Today while I was running in the beautiful nature in New Zealand, I have stumbled upon this trash. I believe that we should do something about it. To let people know that this behavior is not ok.', 1);
 
 INSERT INTO tag_list (tag_name)
-VALUES ('Horsens');
+VALUES ('Polution');
 
 INSERT INTO post_Tag (post_id, tag_name)
-VALUES (1, 'Horsens');
+VALUES (1, 'Polution');
+
+INSERT INTO tag_list (tag_name)
+VALUES ('beach');
+
+INSERT INTO post_Tag (post_id, tag_name)
+VALUES (1, 'beach');
 
 INSERT INTO LikePost (user_id, post_id)
 VALUES (2, 1);
@@ -158,37 +157,25 @@ VALUES (2, 1);
 INSERT INTO LikePost (user_id, post_id)
 VALUES (1, 1);
 
-INSERT INTO comment (body, user_id, post_id)
-VALUES ('Hahaha', 1, 1);
 
 INSERT INTO comment (body, user_id, post_id)
-VALUES ('Nice', 2, 1);
+VALUES ('That is awful!', 2, 1);
 
 INSERT INTO comment (body, user_id, post_id)
-VALUES ('Sub comment', 2, 1);
+VALUES ('Let''s do something about it', 2, 1);
 
 INSERT INTO comment (body, user_id, post_id)
-VALUES ('Sub another one', 2, 1);
-
-INSERT INTO comment (body, user_id, post_id)
-VALUES ('Sub sub comment', 2, 1);
+VALUES ('Let''s contact town hall about that', 1, 1);
 
 INSERT INTO CommentParentComment (comment_id, parent_comment_id)
 VALUES (3, 2);
 
-INSERT INTO CommentParentComment (comment_id, parent_comment_id)
-VALUES (4, 2);
-
-INSERT INTO CommentParentComment (comment_id, parent_comment_id)
-VALUES (5, 3);
 
 INSERT INTO report(violation_desc, post_id, user_id)
 VALUES ('The image is not related to the topic', 1, 1);
 
 INSERT INTO admin (user_name, password, email)
 VALUES ('admin', 'admin123', 'admin@gmail.com');
-
-
 
 drop TRIGGER if exists whenbanpostchanges on ban_post;
 DROP FUNCTION if EXISTS insertIntoBanUserId();
@@ -197,28 +184,31 @@ create FUNCTION insertIntoBanUserId()
 	LANGUAGE plpgsql
 AS $$
 DECLARE userId int;
-    DECLARE count int;
+	DECLARE count int;
 BEGIN
-SELECT user_id
-INTO userId
-FROM post
-WHERE post.id = new.post_id;
+	SELECT user_id
+	INTO userId
+	FROM post
+	WHERE post.id = new.post_id;
 
-select count(*) into count from ban_post where post_id in (SELECT id from post where user_id = userId);
+	select count(*)
+	into count
+	from ban_post
+	where post_id in (SELECT id from post where user_id = userId);
 
-if count >= 3
-            then
-                Insert Into ban_user(reason, admin, user_id)
-                VALUES('more than 3 reported posts', new.admin, userId);
-end if;
+	if count >= 3
+	then
+		Insert Into ban_user(reason, admin, user_id)
+		VALUES('more than 3 reported posts', new.admin, userId);
+	end if;
 
-return new;
+	return new;
 END
-    $$;
+$$;
 
 CREATE TRIGGER whenBanPostChanges
 	AFTER INSERT
 	on "ban_post"
 	for EACH ROW
 --     when
-	EXECUTE PROCEDURE insertIntoBanUserId();
+EXECUTE PROCEDURE insertIntoBanUserId();
